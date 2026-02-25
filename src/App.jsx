cb837e22860a8cdff4bb2c9a311c4294bfe8f641
Grace Hollenbach <llamaladycreates@gmail.com>
2026-02-25 08:14:04 -0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,14 @@
-
-import React, { useState } from 'react'
 import './App.css'
-import Page1 from './components/Page1'
-import Page2 from './components/Page2'
-import Page3 from './components/Page3'
-import Page4 from './components/Page4'
-import Page5 from './components/Page5'
-import Page7 from './components/Page7'
-import Page8 from './components/Page8'
-import Infinite from './components/Infinite.jsx'
-import Page14 from './components/Page14'
-import Page15 from './components/Page15'
-import PageFinal from './components/PageFinal'
-
 
 function App() {
-  const [currentPage, setCurrentPage] = useState(0)
-  
-  const pages = [
-    <Page1 key="page1" onNext={() => setCurrentPage(1)} />,
-    <Page2 key="page2" onNext={() => setCurrentPage(2)} />,
-    <Page3 key="page3" onNext={() => setCurrentPage(3)} />,
-    <Page4 key="page4" onNext={() => setCurrentPage(4)} />,
-    <Page5 key="page5" onNext={() => setCurrentPage(5)} />,
-    <Page7 key="page7" onNext={() => setCurrentPage(6)} />,
-    <Page8 key="page8" onNext={() => setCurrentPage(7)} />,
-    <Infinite key="infinite" onNext={() => setCurrentPage(8)}/>,
-    <Page15 key="page15" onNext={() => setCurrentPage(9)} />,
-    <Page14 key="page14" onNext={() => setCurrentPage(10)} />,
-    <Page15 key="page15" onNext={() => setCurrentPage(11)} />,
-    <PageFinal key="pagefinal" />
-  ]
-
   return (
     <div className="App">
-      {pages[currentPage]}
+      <header className="App-header">
+        <h1>Welcome to Your New Site</h1>
+        <p>Start building something awesome!</p>
+      </header>
     </div>
   )
 }
 
 export default App
-
